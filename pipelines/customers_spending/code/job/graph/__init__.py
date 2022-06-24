142ac2432b5ca2daf2f58a298c1c3513c8d3d391
several27@icloud.com <several27@icloud.com>
2022-06-24 01:54:36 +0000

    cleanup

--- pipelines/customers_spending/code/job/graph/__init__.py
+++
@@ -1,6 +0,0 @@
-from .orders import orders
-from .report import report
-from .customer_details_1 import customer_details_1
-from .sum_amounts import sum_amounts
-from .by_custkey import by_custkey
-from .customers_spendings import customers_spendings
--- pipelines/customers_spending/code/job/graph/customer_details_1/regions.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import *
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-from job.config.ConfigStore import *
-from job.udfs.UDFs import *
-
-def regions(spark: SparkSession) -> DataFrame:
-    return spark.read.format("delta").load("dbfs:/databricks-datasets/tpch/delta-001/region/")
