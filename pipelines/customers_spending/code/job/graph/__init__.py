71f35f75016d2d6a27a73e5e54059d9e3c295264
several27@icloud.com <several27@icloud.com>
2022-06-24 15:55:44 +0000

    cleanup

--- pipelines/customers_spending/code/job/graph/__init__.py
+++
@@ -1,6 +0,0 @@
-from .customers_source import customers_source
-from .orders import orders
-from .report import report
-from .customer_details_1 import customer_details_1
-from .sum_amounts import sum_amounts
-from .by_customer_id import by_customer_id
--- pipelines/customers_spending/code/job/graph/report.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import *
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-from job.config.ConfigStore import *
-from job.udfs.UDFs import *
-
-def report(spark: SparkSession, in0: DataFrame):
-    in0.write.format("delta").mode("overwrite").saveAsTable("maciej.customer_spendings")
