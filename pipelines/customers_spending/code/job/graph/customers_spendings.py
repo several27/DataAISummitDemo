237808582b47529886110d8b194099491617e50b
several27@icloud.com <several27@icloud.com>
2022-06-24 01:45:12 +0000

    some changes

---
+++ pipelines/customers_spending/code/job/graph/customers_spendings.py
@@ -0,0 +1,8 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from job.config.ConfigStore import *
+from job.udfs.UDFs import *
+
+def customers_spendings(spark: SparkSession) -> DataFrame:
+    return spark.read.format("delta").load("dbfs:/databricks-datasets/tpch/delta-001/customer/")
